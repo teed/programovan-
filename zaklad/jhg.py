fb9c75ed07a543cba3cfb71901b2db2fedeaf94d
teed <tadeas1.balik@gmail.com>
2025-11-12 08:23:21 +0100

    hhhh

---
+++ zaklad/jhg.py
@@ -0,0 +1,7 @@
+#na druhý řádek začněte vypracovávat desetiminutovku
+vek=int(input("kolik let"))
+obc=(input("jsi občan čr"))
+if vek>=18 and obc=="ano": 
+    print("muzesvolit")
+else :
+    print("muzes volit")
